36f03a9d4439a58888b808d06cce3086304de58b
seokjeongeum <jeongeumseok910@gmail.com>
2024-02-26 13:47:19 +0900

    hw0

--- hw0_starter_code/models.py
+++ hw0_starter_code/models.py
@@ -70,19 +70,33 @@ class MultiTaskNet(nn.Module):
 
     def __init__(self, num_users, num_items, embedding_dim=32, layer_sizes=[96, 64],
                  sparse=False, embedding_sharing=True):
-
         super().__init__()
 
         self.embedding_dim = embedding_dim
 
-        #********************************************************
-        #******************* YOUR CODE HERE *********************
-        #********************************************************
+        # ********************************************************
+        # ******************* YOUR CODE HERE *********************
+        # ********************************************************
+        self.sparse = sparse
+        self.embedding_sharing = embedding_sharing
 
+        self.U_predictions = ScaledEmbedding(num_users, embedding_dim)
+        self.Q_predictions = ScaledEmbedding(num_items, embedding_dim)
+        self.U_score = ScaledEmbedding(num_users, embedding_dim)
+        self.Q_score = ScaledEmbedding(num_items, embedding_dim)
+        self.A = ZeroEmbedding(num_users, 1)
+        self.B = ZeroEmbedding(num_items, 1)
 
-        #********************************************************
-        #********************************************************
-        #********************************************************
+        self.f = nn.Sequential(
+            nn.Linear(layer_sizes[0], layer_sizes[1]),
+            nn.ReLU(),
+            nn.Linear(layer_sizes[1], embedding_dim),
+            nn.ReLU(),
+            nn.Linear(embedding_dim, 1),
+        )
+        # ********************************************************
+        # ********************************************************
+        # ********************************************************
 
     def forward(self, user_ids, item_ids):
         """
@@ -104,16 +118,37 @@ class MultiTaskNet(nn.Module):
         score: tensor
             Tensor of user-item score predictions of shape (batch,)
         """
-        #********************************************************
-        #******************* YOUR CODE HERE *********************
-        #********************************************************
+        # ********************************************************
+        # ******************* YOUR CODE HERE *********************
+        # ********************************************************
+        u_predictions = self.U_predictions(user_ids)
+        q_predictions = self.Q_predictions(item_ids)
+        a = self.A(user_ids)
+        b = self.B(item_ids)
 
+        u_score = self.U_score(user_ids)
+        q_score = self.Q_score(item_ids)
 
-        #********************************************************
-        #********************************************************
-        #********************************************************
+        if self.embedding_sharing:
+            u_score = self.U_predictions(user_ids)
+            q_score = self.Q_predictions(item_ids)
+
+        if self.sparse:
+            u_predictions = u_predictions.to_sparse()
+            q_predictions = q_predictions.to_sparse()
+            a = a.to_sparse()
+            b = b.to_sparse()
+
+            u_score = u_score.to_sparse()
+            q_score = q_score.to_sparse()
+        predictions = (u_predictions * q_predictions).sum(axis=1) + a.reshape(-1) + b.reshape(-1)
+
+        score = self.f(torch.cat([u_score, q_score, u_score * q_score], dim=1)).reshape(-1)
+        # ********************************************************
+        # ********************************************************
+        # ********************************************************
         ## Make sure you return predictions and scores of shape (batch,)
         if (len(predictions.shape) > 1) or (len(score.shape) > 1):
             raise ValueError("Check your shapes!")
-        
+
         return predictions, score
